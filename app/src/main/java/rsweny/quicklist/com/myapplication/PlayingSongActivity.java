915957716e377af4157ac66fb7a613a5be93941f
Rob Sweny <Robsweny@gmail.com>
2019-03-10 02:44:20 +0000

    added playing_song activity new images added for the media linked both activities hidden menu item when switching between activities updated strings buttons on playing_song activity linked and tested

--- app/src/main/java/rsweny/quicklist/com/myapplication/PlayingSongActivity.java
+++ app/src/main/java/rsweny/quicklist/com/myapplication/PlayingSongActivity.java
@@ -1,6 +1,5 @@
 package rsweny.quicklist.com.myapplication;
 
-import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -8,10 +7,10 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-
-public class MainActivity extends AppCompatActivity {
+public class PlayingSongActivity extends AppCompatActivity {
 
     // Declaring views
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -22,30 +21,46 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_music_player:
+                    Intent intent = new Intent(PlayingSongActivity.this, MainActivity.class);
+                    startActivity(intent);
                     break;
 
                 case R.id.navigation_play_song:
-                    Intent intent = new Intent(MainActivity.this, PlayingSongActivity.class);
-                    startActivity(intent);
                     break;
             } // End Switch
             return false;
         }
     };
 
+    // Declare buttons
+    private Button previous_song_button;
+    private Button play_pause_song_button;
+    private Button next_song_button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.playing_song_activity);
 
         // Initializing
         BottomNavigationView bottom_navigation = findViewById(R.id.bottom_navigation);
         bottom_navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        bottom_navigation.getMenu().findItem(R.id.navigation_music_player).setVisible(false);
+        bottom_navigation.getMenu().findItem(R.id.navigation_play_song).setVisible(false);
+        previous_song_button = findViewById(R.id.previous_song_button);
+        play_pause_song_button = findViewById(R.id.previous_song_button);
+        next_song_button = findViewById(R.id.previous_song_button);
+
     } // End onCreate
 
-
-
-} // End Main Activity
+    public void songChange(View view) {
+        if (view.getId() == R.id.previous_song_button) {
+            Toast.makeText(this,"PREVIOUS",Toast.LENGTH_SHORT).show();
+        } else if (view.getId() == R.id.play_pause_song_button) {
+            Toast.makeText(this,"PLAY|PAUSE",Toast.LENGTH_SHORT).show();
+        } else if (view.getId() == R.id.next_song_button) {
+            Toast.makeText(this,"NEXT",Toast.LENGTH_SHORT).show();
+        }
+    } // End songChange
+}
